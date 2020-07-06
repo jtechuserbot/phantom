8b336c73cffa2e1443072a9ce844da66668c58e0
jtechuserbot <65949382+jtechuserbot@users.noreply.github.com>
2020-07-06 21:35:36 +0300

    Update Ammu.py

--- tg_bot/modules/Ammu.py
+++ tg_bot/modules/Ammu.py
@@ -44,6 +44,6 @@ __help__ = """
 
 __mod_name__ = "Ammu"
 
-DARK_HANDLER = DisableAbleCommandHandler("ammu", ammu)
+AMMU_HANDLER = DisableAbleCommandHandler("ammu", ammu)
 
-dispatcher.add_handler(DARK_HANDLER)
+dispatcher.add_handler(AMMU_HANDLER)
