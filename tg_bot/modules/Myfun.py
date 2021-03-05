12a7775e272089102b459d960754a65f4bdbaf08
jtechuserbot <65949382+jtechuserbot@users.noreply.github.com>
2021-03-05 04:21:23 +0300

    Add files via upload

---
+++ tg_bot/modules/Myfun.py
@@ -0,0 +1,60 @@
+import html
+import random
+import time
+from typing import List
+
+from telegram import Bot, Update, ParseMode
+from telegram.ext import run_async
+
+import tg_bot.modules.Myfunstrings as Myfunstrings
+from tg_bot import dispatcher
+from tg_bot.modules.disable import DisableAbleCommandHandler
+from tg_bot.modules.helper_funcs.chat_status import is_user_admin
+from tg_bot.modules.helper_funcs.extraction import extract_user
+
+
+@run_async
+def cyp(bot: Bot,update: Update, args: List[str]):
+    message = update.effective_message
+    chat = update.effective_chat
+
+    reply_text = message.reply_to_message.reply_text if message.reply_to_message else message.reply_text
+
+    curr_user = html.escape(message.from_user.first_name)
+    user_id = extract_user(message, args)
+
+    if user_id == 1279942651:
+        temp = random.choice(Myfunstrings.JINN_SAITAMA_TEMPLATES)
+
+        if isinstance(temp, list):
+            if temp[2] == "tmute":
+                if is_user_admin(chat, message.from_user.id):
+                    reply_text(temp[1])
+                    return
+
+                mutetime = int(time.time() + 60)
+                bot.restrict_chat_member(chat.id, message.from_user.id, until_date=mutetime, can_send_messages=False)
+            reply_text(temp[0])
+        else:
+            reply_text(temp)
+        return
+
+    if user_id:
+
+        slapped_user = bot.get_chat(user_id)
+        user1 = curr_user
+        user2 = html.escape(slapped_user.first_name)
+
+    else:
+        user1 = bot.first_name
+        user2 = curr_user
+
+    temp = random.choice(Myfunstrings.JINN_TEMPLATES)
+    item = random.choice(fun_strings.ITEMS)
+    hit = random.choice(fun_strings.HIT)
+    throw = random.choice(fun_strings.THROW)
+
+    reply = temp.format(user1=user1, user2=user2, item=item, hits=hit, throws=throw)
+
+    reply_text(reply, parse_mode=ParseMode.HTML)
+
